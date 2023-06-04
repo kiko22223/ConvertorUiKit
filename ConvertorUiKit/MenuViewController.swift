226b3f11276dc5782b15535d503e17fb28034624
kiko22223 <63966405+kiko22223@users.noreply.github.com>
2023-06-04 19:26:15 +0200

    added next unit datas

--- ConvertorUiKit/MenuViewController.swift
+++ ConvertorUiKit/MenuViewController.swift
@@ -16,7 +16,11 @@ class MenuViewController: UIViewController {
                                                   sections: [Section(title: "Metrické", units: [0,1,2,3]),
                                                              Section(title: "Imperiálne", units: [4,5,6,7])],
                                                   ratios: [1000, 1, 0.01, 0.001, 1609.344, 0.9144, 0.3048, 0.0254])),
-        MenuTableViewCellDescription(title: "Volume", imageName: "imageML", gradientStartColor: .orange, gradientEndColor: .yellow),
+        MenuTableViewCellDescription(title: "Volume", imageName: "imageML", gradientStartColor: .orange, gradientEndColor: .yellow,
+                                     units: Units(units: ["l","ml","cup","pt","gl"],
+                                                  sections: [Section(title: "Metrické", units: [0,1]),
+                                                             Section(title: "Imperiálne", units: [2,3,4])],
+                                                  ratios: [1000,1,236.588237,473.176473,3785.41178])),
         MenuTableViewCellDescription(title: "Weight", imageName: "imageKG",gradientStartColor: .red, gradientEndColor: .blue),
         MenuTableViewCellDescription(title: "Temperature", imageName: "imageC",gradientStartColor: UIColor.blue, gradientEndColor: UIColor.purple)
         ]
