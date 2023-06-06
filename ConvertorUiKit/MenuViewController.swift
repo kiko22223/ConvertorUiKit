5018ab81547d2c8cf719ccc61bfb9a225c26cdeb
kiko22223 <63966405+kiko22223@users.noreply.github.com>
2023-06-06 21:31:29 +0200

    Added units for Teplota, Vaha,

--- ConvertorUiKit/MenuViewController.swift
+++ ConvertorUiKit/MenuViewController.swift
@@ -13,18 +13,24 @@ class MenuViewController: UIViewController {
         MenuTableViewCellDescription(title: "Lenght", imageName: "imageKM",
                                      gradientStartColor: .blue, gradientEndColor: .cyan,
                                      units: Units(units: ["km", "m", "cm", "mm", "Mi", "Yd", "Ft", "In"],
-                                                  sections: [Section(title: "Metrické", units: [0,1,2,3]),
-                                                             Section(title: "Imperiálne", units: [4,5,6,7])],
+                                                  sections: [Section(title: "Metrické", units: [0, 1, 2, 3]),
+                                                             Section(title: "Imperiálne", units: [4, 5, 6, 7])],
                                                   ratios: [1000, 1, 0.01, 0.001, 1609.344, 0.9144, 0.3048, 0.0254])),
         MenuTableViewCellDescription(title: "Volume", imageName: "imageML", gradientStartColor: .orange, gradientEndColor: .yellow,
-                                     units: Units(units: ["l","ml","cup","pt","gl"],
-                                                  sections: [Section(title: "Metrické", units: [0,1]),
-                                                             Section(title: "Imperiálne", units: [2,3,4])],
-                                                  ratios: [1000,1,236.588237,473.176473,3785.41178])),
-        MenuTableViewCellDescription(title: "Weight", imageName: "imageKG",gradientStartColor: .red, gradientEndColor: .blue),
-        MenuTableViewCellDescription(title: "Temperature", imageName: "imageC",gradientStartColor: UIColor.blue, gradientEndColor: UIColor.purple)
-        ]
-
+                                     units: Units(units: ["l", "ml", "cup", "pt", "gl"],
+                                                  sections: [Section(title: "Metrické", units: [0, 1]),
+                                                             Section(title: "Imperiálne", units: [2, 3, 4])],
+                                                  ratios: [1000, 1,236.588237, 473.176473, 3785.41178])),
+        MenuTableViewCellDescription(title: "Weight", imageName: "imageKG"
+                                     ,gradientStartColor: .red, gradientEndColor: .blue,
+                                     units: Units(units: ["mg","g", "dk", "kg", "q", "T"],
+                                                  sections: [Section(title: "Metrické", units: [0, 1, 2, 3,]),
+                                                            Section(title: "Imperiálne", units: [4, 5])],
+                                                  ratios: [1, 1000, 10000, 1000000, 100000000, 1000000000 ])),
+        MenuTableViewCellDescription(title: "Temperature", imageName: "imageC",gradientStartColor: UIColor.blue, gradientEndColor: UIColor.purple,
+                                     units: Units(units: ["J", "Nm", "mJ", "kJ", "Mj", "Gj", "Tj"],
+                                                 sections: [Section(title: "Teplota", units: [0, 1, 2, 3, 4, 5, 6])],
+                                                 ratios: [1, 1000, 1000, 1000000, 1000000000, 1_000_000_000_000, 1_000_000_000_000_000]))]
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
